972e8e06f16e16d2ffdd752647184b98c2b04df9
Cherry Bomb <110393598+Ch3rryB0m@users.noreply.github.com>
2023-05-09 19:00:37 +0200

    main menu and difficulty done ~Sky

--- HideFromHumansUI/Assets/Scripts/mainMenu.cs
+++ HideFromHumansUI/Assets/Scripts/mainMenu.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void startDefault()
     {
-        
+        SceneManager.LoadScene("easyMode");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void startEasy()
     {
-        
+        SceneManager.LoadScene("easyMode");
+    }
+    public void startMedium()
+    {
+        SceneManager.LoadScene("mediumMode");
+    }
+    public void startHard()
+    {
+        SceneManager.LoadScene("hardMode");
     }
 }
