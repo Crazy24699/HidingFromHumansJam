841fec94cce7350c9287d39e9ebafcae1ad9a3eb
Crazy24699 <carlvschulze2@gmail.com>
2023-05-15 13:03:36 +0200

    Materials and assets

---
+++ Hiding from humans/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void startDefault()
+    {
+        SceneManager.LoadScene("easyMode");
+    }
+    public void startEasy()
+    {
+        SceneManager.LoadScene("easyMode");
+    }
+    public void startMedium()
+    {
+        SceneManager.LoadScene("mediumMode");
+    }
+    public void startHard()
+    {
+        SceneManager.LoadScene("hardMode");
+    }
+}
